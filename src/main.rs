dba09ab22ab7734c17ef2b547239abe3f048cd5d
Adit <aditya.umakanth@gmail.com>
2023-09-19 21:09:39 -0500

    slightly refactored main

--- src/main.rs
+++ src/main.rs
@@ -19,35 +19,32 @@ type Reciever = SplitStream<WebSocketStream<TcpStream>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut table = SpeedTable::new();
-    table.flip_middle_cards();
-    table.player_draw_card(Player::PLAYER1);
-    table.player_draw_card(Player::PLAYER1);
-    table.player_draw_card(Player::PLAYER1);
-    table.player_draw_card(Player::PLAYER1);
-
-    table.player_draw_card(Player::PLAYER2);
-    table.player_draw_card(Player::PLAYER2);
-    table.player_draw_card(Player::PLAYER2);
-    table.player_draw_card(Player::PLAYER2);
-
     let addr = "0.0.0.0:8080".to_string();
     let listener = TcpListener::bind(&addr).await?;
 
-    let (mut p1_tx, mut p1_rx) = connect_player(&listener).await?;
+    let p1 = connect_player(&listener).await?;
     println!("Player 1 connected!");
-    let (mut p2_tx, mut p2_rx) = connect_player(&listener).await?;
+    let p2 = connect_player(&listener).await?;
     println!("Player 2 connected!");
 
-    send_player_message(&mut p1_tx, &mut p2_tx, &table, ServerAction::SetBoard).await;
+    start_game(p1, p2).await?;
+
+    Ok(())
+}
+
+async fn start_game(mut p1: (Sender, Reciever), mut p2: (Sender, Reciever)) -> Result<()> {
+    let mut table = SpeedTable::new();
+    send_player_message(&mut p1.0, &mut p2.0, &table, ServerAction::SetBoard).await;
 
     loop {
-        let (player_move, player) = wait_for_player_move(&mut p1_rx, &mut p2_rx).await?;
-        println!("{:#?} by {:#?}", player_move, player);
+        let (player_move, player) = wait_for_player_move(&mut p1.1, &mut p2.1).await?;
 
         match player_move {
             PlayerAction::DrawCard => {
-                table.player_draw_card(player);
+                match table.player_draw_card(player) {
+                    Ok(_) => todo!(),
+                    Err(_) => todo!(),
+                };
             }
             PlayerAction::Flip => {
                 table.flip_middle_cards();
@@ -56,10 +53,8 @@ async fn main() -> Result<()> {
                 table.place_card(player, side, hand_index);
             }
         }
-        send_player_message(&mut p1_tx, &mut p2_tx, &table, ServerAction::SetBoard).await;
+        send_player_message(&mut p1.0, &mut p2.0, &table, ServerAction::SetBoard).await;
     }
-
-    Ok(())
 }
 
 async fn connect_player(listener: &TcpListener) -> Result<(Sender, Reciever)> {
